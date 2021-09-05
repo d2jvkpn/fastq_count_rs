1443c6eae2695b7e9ff21f69e06e47bc9906befa
d2jvkpn <chenbin2018@protonmail.com>
2021-09-05 12:18:23 +0800

    Config from arguments

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::process;
 
 use chrono::prelude::*;
-use clap::{App, Arg};
+use clap::{App, Arg, Values};
 
 mod fq_count;
 
@@ -11,6 +11,15 @@ extern crate serde_derive;
 // https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-crates
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 const AUTHORS: &'static str = env!("CARGO_PKG_AUTHORS");
+
+#[derive(Debug)]
+struct Config<'a> {
+    inputs: Vec<&'a str>,
+    phred: u8,
+    output: &'a str,
+    json_fmt: bool,
+    debug: bool,
+}
 
 fn main() {
     //##
@@ -27,12 +36,6 @@ fn main() {
         .default_value("33")
         .help("phred value");
 
-    let json_arg = Arg::with_name("json")
-        .long("json")
-        .takes_value(false)
-        .required(false)
-        .help("output in json format");
-
     let output_arg = Arg::with_name("output")
         .long("output")
         .takes_value(true)
@@ -40,27 +43,52 @@ fn main() {
         .default_value("")
         .help("output to file");
 
-    let args = App::new("fastq(https://en.wikipedia.org/wiki/FASTQ_format) count in rust")
+    let json_arg = Arg::with_name("json")
+        .long("json")
+        .takes_value(false)
+        .required(false)
+        .help("output in json format");
+
+    let debug_arg = Arg::with_name("debug")
+        .long("debug")
+        .takes_value(false)
+        .required(false)
+        .help("run in debug mode");
+
+    let matches = App::new("fastq(https://en.wikipedia.org/wiki/FASTQ_format) count in rust")
         .about("count fastq reads, bases, N Bases, Q20, Q30, GC")
         .author(AUTHORS)
         .version(VERSION)
         .set_term_width(100)
         .arg(inputs_arg)
         .arg(phred_arg)
-        .arg(json_arg)
         .arg(output_arg)
+        .arg(json_arg)
+        .arg(debug_arg)
         .get_matches();
 
-    let phred = args.value_of("phred").unwrap().parse::<u8>().unwrap();
-    let inputs = args.values_of("inputs").unwrap();
-    let json_fmt = args.is_present("json");
-    let output = args.value_of("output").unwrap();
+    // let inputs = args.values_of("inputs");
+    let config = Config {
+        inputs: matches
+            .values_of("inputs")
+            .map(Values::collect)
+            .unwrap_or_else(|| vec![]),
+
+        phred: matches.value_of("phred").unwrap().parse::<u8>().unwrap(),
+        output: matches.value_of("output").unwrap(),
+        json_fmt: matches.is_present("json"),
+        debug: matches.is_present("debug"),
+    };
+
+    if config.debug {
+        dbg!(&config);
+    }
 
     //##
-    let mut fqc = fq_count::FQCount::new(phred);
+    let mut fqc = fq_count::FQCount::new(config.phred);
     let start: DateTime<Local> = Local::now();
 
-    for input in inputs {
+    for input in config.inputs {
         let local: DateTime<Local> = Local::now();
 
         eprintln!(
@@ -69,8 +97,8 @@ fn main() {
             input
         );
 
-        match fastq_count_rs::read_input(input) {
-            Ok(buf_read) => match fq_count::read(buf_read, phred) {
+        match fastq_count_rs::read_input(&input) {
+            Ok(buf_read) => match fq_count::read(buf_read, config.phred) {
                 Ok(y) => fqc.add(y),
                 Err(err) => panic!("fq_count::read {}: {:?}", input, err),
             },
@@ -82,7 +110,6 @@ fn main() {
 
         let log_elapsed = || {
             let end: DateTime<Local> = Local::now();
-            // let dura = end.signed_duration_since(start);
             eprintln!(
                 "{} fastq count elapsed: {:?}",
                 end.to_rfc3339_opts(SecondsFormat::Millis, true),
@@ -91,6 +118,6 @@ fn main() {
         };
 
         log_elapsed();
-        fqc.output(output, json_fmt).unwrap();
+        fqc.output(config.output, config.json_fmt).unwrap();
     }
 }
