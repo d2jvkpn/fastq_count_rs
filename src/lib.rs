ba07dc2c470d2fd2a4d42c4149caf1db2cb1c944
d2jvkpn <chenbin2018@protonmail.com>
2021-09-05 11:18:12 +0800

    ...

--- src/lib.rs
+++ src/lib.rs
@@ -11,9 +11,16 @@ pub fn read_input(input: &str) -> Result<Box<dyn BufRead>, Box<dyn error::Error>
     let file = fs::File::open(input)?;
     let reader = io::BufReader::new(file);
 
+    /*
     if input.ends_with(".gz") {
         return Ok(Box::new(io::BufReader::new(GzDecoder::new(reader))));
     }
 
     return Ok(Box::new(reader));
+    */
+
+    match input {
+        input if input.ends_with(".gz") => Ok(Box::new(io::BufReader::new(GzDecoder::new(reader)))),
+        _ => Ok(Box::new(reader)),
+    }
 }
