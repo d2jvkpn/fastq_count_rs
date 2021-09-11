3df36c485a0cff90ef676ef8b5c7b14006867e91
d2jvkpn <chenbin2018@protonmail.com>
2021-09-11 21:47:33 +0800

    ...

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,23 @@ use std::process;
 use fastq_count::{get_args, run};
 
 fn main() {
-    if let Err(e) = get_args().and_then(run) {
-        eprintln!("{}", e);
+    /*
+    if let Err(err) = get_args().and_then(run) {
+        eprintln!("{}", err);
+        process::exit(1);
+    }
+    */
+
+    let config = match get_args() {
+        Ok(data) => data,
+        Err(err) => {
+            eprintln!("get_args: {}", err);
+            process::exit(1);
+        }
+    };
+
+    if let Err(err) = run(config) {
+        eprintln!("run: {}", err);
         process::exit(1);
     }
 }
